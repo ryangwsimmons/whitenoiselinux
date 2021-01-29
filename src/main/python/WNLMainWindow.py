f0a659ba3629f879227f43abf7c2b8094581434e
Ryan Simmons <simmonsrgw@gmail.com>
2021-01-28 23:57:09 -0500

    Added sound picker UI

--- src/main/python/WNLMainWindow.py
+++ src/main/python/WNLMainWindow.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import (QMainWindow, QVBoxLayout, QHBoxLayout, QPushButton, QWidget)
+from PyQt5.QtWidgets import (QMainWindow, QVBoxLayout, QHBoxLayout, QPushButton, QWidget, QGridLayout, QLabel, QListWidget)
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import Qt
 from WNLAboutDialog import WNLAboutDialog
@@ -37,6 +37,16 @@ class WNLMainWindow(QMainWindow):
         # Add the top bar to the main layout
         self.mainLayout.addLayout(self.topBarLayout)
 
+        # Create sound selection layout
+        self.soundSelectionLayout = QGridLayout()
+        self.soundSelectionLayout.setAlignment(Qt.AlignHCenter)
+
+        # Populate sound selection layout
+        self.populateSoundSelect()
+
+        # Add sound selection to main layout
+        self.mainLayout.addLayout(self.soundSelectionLayout)
+
         # Set the main layout as the layout for the window
         self.centralWidget.setLayout(self.mainLayout)
         self.setCentralWidget(self.centralWidget)
@@ -53,4 +63,51 @@ class WNLMainWindow(QMainWindow):
     def openAboutDialog(self):
         aboutDialog = WNLAboutDialog(self.appctxt)
         aboutDialog.exec_()
+
+    def populateSoundSelect(self):
+        # Create and populate the QVBoxLayout for selecting available sounds
+        availSoundsLayout = QVBoxLayout()
+
+        availSoundsLabel = QLabel("Available Sounds:")
+        availSoundsSelect = QListWidget()
+
+        availSoundsLayout.addWidget(availSoundsLabel)
+        availSoundsLayout.addWidget(availSoundsSelect)
+
+        # Create and populate the QVBoxLayout for moving sounds to the currently playing box
+        moveSoundsWidget = QWidget()
+        moveSoundsWidget.setFixedHeight(100)
+
+        moveSoundsLayout = QVBoxLayout()
+
+        addSound = QPushButton()
+        addSound.setFixedSize(75, 25)
+        addSound.setIcon(QIcon.fromTheme("go-next"))
+        addSound.setToolTip("Add Sound to Currently Playing")
+        addSound.setEnabled(False)
+
+        rmSound = QPushButton()
+        rmSound.setFixedSize(75, 25)
+        rmSound.setIcon(QIcon.fromTheme("go-previous"))
+        rmSound.setToolTip("Remove Sound from Currently Playing")
+        rmSound.setEnabled(False)
+
+        moveSoundsLayout.addWidget(addSound)
+        moveSoundsLayout.addWidget(rmSound)
+
+        moveSoundsWidget.setLayout(moveSoundsLayout)
+
+        # Create and populate the QVBoxLayout for displaying currently playing sounds
+        currSoundsLayout = QVBoxLayout()
+
+        currSoundsLabel = QLabel("Currently Playing:")
+        currSoundsSelect = QListWidget()
+
+        currSoundsLayout.addWidget(currSoundsLabel)
+        currSoundsLayout.addWidget(currSoundsSelect)
+
+        # Add all the layouts to the sound selection layout
+        self.soundSelectionLayout.addLayout(availSoundsLayout, 0, 0, 1, 1)
+        self.soundSelectionLayout.addWidget(moveSoundsWidget, 0, 1, 1, 1)
+        self.soundSelectionLayout.addLayout(currSoundsLayout, 0, 2, 1, 1)
 
